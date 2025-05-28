cc44aba88bdd5fd3e83c03a702442869897c89ee
Bryan Josue Vivanco SIlva <106763701+bryanvivancos@users.noreply.github.com>
2025-05-28 08:33:36 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="text-white font-Quick flex flex-col items-center justify-center gap-0 mb-4 mt-10">
             <a href="https://github.com/bryanvivancos/bjlinks-react-tailwind"
             target="_blank">
-                <p className="text-sm">
+                <p className="text-sm text-center">
                     © 2020-{year} <span>Desarrollado con ❤️ por Bryan Vivanco</span>
                 </p>
             </a>
