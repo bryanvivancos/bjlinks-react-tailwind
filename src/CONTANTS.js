fd0c28065631378746c38e7e5c7e87837ff1f7bc
Bryan Josue Vivanco SIlva <106763701+bryanvivancos@users.noreply.github.com>
2025-09-27 22:49:04 -0500

    Update CONTANTS.js

--- src/CONTANTS.js
+++ src/CONTANTS.js
@@ -1,4 +1,4 @@
-export const DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado de la Universidad Nacional de Piura. Autodidacta con +3 años trabajando en el campo de TI y +1 año aprendiendo tecnologías y realizando proyectos personales. También cuento con buenas habilidades en Ingeniería de datos."
+export const DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado de la Universidad Nacional de Piura. Autodidacta con +4 años en el campo de TI y +3 años trabajando con variedad de tecnologías de desarrollo y realizando proyectos personales. También cuento con buenas habilidades en Ingeniería de datos."
 
 export const FOOTER_DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado de la Universidad Nacional de Piura";
 
