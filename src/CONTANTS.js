ba11a63c1c87cb5ffacabd5a118ec74755a4b245
bryanvivancos <bryanvivanco97@outlook.com>
2026-01-07 15:58:25 -0500

    add: metatags

--- src/CONTANTS.js
+++ src/CONTANTS.js
@@ -1,6 +1,6 @@
-export const DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado de la Universidad Nacional de Piura. Autodidacta con +4 años en el campo de TI y +3 años trabajando con variedad de tecnologías de desarrollo y realizando proyectos personales. También cuento con buenas habilidades en Ingeniería de datos."
+export const DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado. Con +4 años en el campo de TI, trabajando con variedad de tecnologías de desarrollo, realizando proyectos personales y para clientes. Aquí podrás encontrar todos mis enlaces de interés."
 
-export const FOOTER_DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado de la Universidad Nacional de Piura";
+export const FOOTER_DESCRIPTION = "Desarrollador Web e Ingeniero Electrónico y Telecomunicaciones Colegiado";
 
 export const DIGITAL_CARD = "https://bryanvivancosbusiness.vercel.app/"
 
